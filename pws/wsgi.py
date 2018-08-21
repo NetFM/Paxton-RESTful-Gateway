c8f919ec3eb72a543dc5a2d638b598c290acec33
David Herring <dave@netfm.org>
2018-08-21 13:37:00 +0100

    Making the names clearer, all the C# code done by Colin is in gateway

--- pws/wsgi.py
+++ pws/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for paxton project.
+WSGI config for pws project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paxton.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pws.settings')
 
 application = get_wsgi_application()
--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/dave/PycharmProjects/paxton/venv/bin/python
+#!/Users/dave/PycharmProjects/pws/venv/bin/python
 from django.core import management
 
 if __name__ == "__main__":
